5ce8d6ca081f51a716ad63964c249032cf247860
E A <ea@example.com>
2025-06-25 09:36:53 -0500

    test webapp

--- webapp/frontend/public/config.js
+++ webapp/frontend/public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
-  "API_URL": "https://lzq5jfiv9b.execute-api.us-east-1.amazonaws.com/Prod",
-  "BUILD_TIME": "2025-06-24T18:31:00.000Z",
+  "API_URL": "https://ye9syrnj8c.execute-api.us-east-1.amazonaws.com/dev",
+  "BUILD_TIME": "2025-06-25T00:00:00.000Z",
   "VERSION": "1.0.0"
 };
