eec85080fd78fc5644c621c839954b989eae5e53
stocks <stocks@localhost>
2025-07-02 16:51:39 -0500

    test deploy

--- webapp/frontend/src/pages/SentimentAnalysis.jsx
+++ webapp/frontend/src/pages/SentimentAnalysis.jsx
@@ -420,7 +420,7 @@ const mockSentimentData = {
         timestamp: '3 hours ago'
       }
     ]
-  }
+  },
   distribution: [
     { sentiment: 'Bullish', percentage: 45 },
     { sentiment: 'Neutral', percentage: 35 },
